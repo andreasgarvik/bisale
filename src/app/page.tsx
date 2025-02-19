9270a41580d071557f91190535dfcbdf654cc8f5
Andreas Garvik <andreas.garvik@sonat.no>
2025-02-19 22:46:51 +0100

    padding and text adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col items-center row-start-2">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen sm:p-40 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col items-center row-start-1">
         <Image
           src="/bisale_logo.svg"
           alt="BISALE logo"

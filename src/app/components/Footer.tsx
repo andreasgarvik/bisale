f4c1f1fa6d13f4ebfb89fb8c54ad471519e35c6c
Andreas Garvik <andreas.garvik@sonat.no>
2025-02-19 22:58:04 +0100

    css adjustments

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,10 +2,8 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="row-start-3 flex flex-col gap-4 flex-wrap items-center justify-center">
-      <div className="text-center text-sm pb-8">
-        All rights reserved © 2025 BISALE
-      </div>
+    <footer className="text-center text-sm pb-4">
+      All rights reserved © 2025 BISALE
     </footer>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen sm:p-40 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col items-center row-start-1">
+    <main className="min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <div className="flex flex-col items-center pt-32">
         <Image
           src="/bisale_logo.svg"
           alt="BISALE logo"
@@ -14,13 +14,13 @@ export default function Home() {
         <div className="text-lg">
           Kompetanse
         </div>
-      </main>
-      <div className="row-start-3 flex flex-col gap-4 flex-wrap items-center justify-center pb-16">
+      </div>
+      <div className="flex flex-col gap-4 flex-wrap items-center pt-96">
         <div className="text-sm">
           Ta kontakt med oss
         </div>
         <a href="mailto:post@bisale.no">post@bisale.no</a>
       </div>
-    </div>
+    </main>
   );
 }
